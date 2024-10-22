5f3689c17c70d3b331bf020308f12ce49f53b018
Daniel Perez Alonso <danielperezalonso30@gmail.com>
2024-10-22 20:04:05 +0200

    navbar cambiado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive, RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLinkActive, RouterOutlet],
+  imports: [RouterOutlet,RouterLinkActive, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
