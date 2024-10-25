53697d34cbdf75289689e8aead0e4fc2c8772f82
Rafael Aznar <rafaaznar@gmail.com>
2024-10-25 18:28:38 +0200

    componente menu contextual

--- src/app/component/shared/shared.menu.unrouted/shared.menu.unrouted.component.ts
+++ src/app/component/shared/shared.menu.unrouted/shared.menu.unrouted.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-shared-menu-unrouted',
   templateUrl: './shared.menu.unrouted.component.html',
   styleUrls: ['./shared.menu.unrouted.component.css'],
-  standalone: true
+  standalone: true,
 })
 export class SharedMenuUnroutedComponent implements OnInit {
+  strRuta: string = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private oRouter: Router) {
+    this.oRouter.events.subscribe((oEvent) => {
+      if (oEvent instanceof NavigationEnd) {
+        this.strRuta = oEvent.url;
+      }
+    });
   }
 
+  ngOnInit() {}
 }
