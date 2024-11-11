0aaa325bdeb1cf3a2113ee64fb2de5c3076a76f3
Alesia <alesiasera04@gmail.com>
2024-11-11 15:56:02 +0100

    modelo y service

---
+++ src/app/model/asiento.interface.ts
@@ -0,0 +1,13 @@
+export interface IAsiento {
+  id: number;
+  descripcion: string;
+  comentarios: string;
+  inventariable: number;
+  id_tipoasiento: number;
+  id_usuario: number;
+  id_periodo: number;
+}
+
+
+
+
