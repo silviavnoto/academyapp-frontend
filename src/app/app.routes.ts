c4058576c569b4e7285f4ccb7a95c9c1794ca582
Rafael Aznar <rafaaznar@gmail.com>
2024-12-02 19:41:43 +0100

    refactor: add new routed components for Apunte, Asiento, Balance, and Cuenta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,9 @@ import { ApunteAdminPlistRoutedComponent } from './component/apunte/apunte.admin
 import { ApunteAdminEditRoutedComponent } from './component/apunte/apunte.admin.edit.routed/apunte.admin.edit.routed.component';
 import { ApunteAdminViewRoutedComponent } from './component/apunte/apunte.admin.view.routed/apunte.admin.view.routed.component';
 import { ApunteAdminCreateRoutedComponent } from './component/apunte/apunte.admin.create.routed/apunte.admin.create.routed.component';
+import { ApunteXAsientoAdminPlistRoutedComponent } from './component/apunte/apunte.xasiento.admin.plist.routed/apunte.xasiento.admin.plist.routed.component';
+import { ApunteXSubcuentaAdminPlistRoutedComponent } from './component/apunte/apunte.xsubcuenta.admin.plist.routed/apunte.xsubcuenta.admin.plist.routed.component';
+import { ApunteXTipoapunteAdminPlistRoutedComponent } from './component/apunte/apunte.xtipoapunte.admin.plist.routed /apunte.xtipoapunte.admin.plist.routed.component';
 
 import { TipoCuentaAdminEditRoutedComponent } from './component/tipocuenta/tipocuenta.admin.edit.routed/tipoCuenta.admin.edit.routed.component';
 import { TipoCuentaAdminViewRoutedComponent } from './component/tipocuenta/tipocuenta.admin.view.routed/tipoCuenta.admin.view.routed.component';
@@ -37,44 +40,38 @@ import { AsientoAdminCreateRoutedComponent } from './component/asiento/asiento.a
 import { AsientoAdminViewRoutedComponent } from './component/asiento/asiento.admin.view.routed/asiento.admin.view.routed.component';
 import { AsientoAdminEditRoutedComponent } from './component/asiento/asiento.admin.edit.routed/asiento.admin.edit.routed.component';
 import { AsientoAdminPlistRoutedComponent } from './component/asiento/asiento.admin.plist.routed/asiento.admin.plist.routed.component';
+import { AsientoXTipoasientoAdminPlistRoutedComponent } from './component/asiento/asiento.xtipoasiento.admin.plist.routed/asiento.xtipoasiento.admin.plist.routed.component';
+import { AsientoXUsuarioAdminPlistRoutedComponent } from './component/asiento/asiento.xusuario.admin.plist.routed/asiento.xusuario.admin.plist.routed.component';
+import { AsientoXPeriodoAdminPlistRoutedComponent } from './component/asiento/asiento.xperiodo.admin.plist.routed/asiento.xperiodo.admin.plist.routed.component';
 
 import { TipoasientoAdminPlistRoutedComponent } from './component/tipoasiento/tipoasiento.admin.plist.routed/tipoasiento.admin.plist.routed.component';
 import { TipoasientoAdminEditRoutedComponent } from './component/tipoasiento/tipoasiento.admin.edit.routed/tipoasiento.admin.edit.routed.component';
 import { TipoasientoAdminViewRoutedComponent } from './component/tipoasiento/tipoasiento.admin.view.routed/tipoasiento.admin.view.routed.component';
 import { TipoasientoAdminCreateRoutedComponent } from './component/tipoasiento/tipoasiento.admin.create.routed/tipoasiento.admin.create.routed.component';
 import { TipoasientoAdminDeleteRoutedComponent } from './component/tipoasiento/tipoasiento.admin.delete.routed/tipoasiento.admin.delete.routed.component';
-import { CuentaAdminCreateRoutedComponent } from './component/cuenta/cuenta.admin.create.routed/cuenta.admin.create.routed.component';
 
 import { BalanceAdminDeleteRoutedComponent } from './component/balance/balance.admin.delete.routed/balance.admin.delete.routed.component';
 import { BalanceAdminCreateRoutedComponent } from './component/balance/balance.admin.create.routed/balance.admin.create.routed.component';
 import { BalanceAdminViewRoutedComponent } from './component/balance/balance.admin.view.routed/balance.admin.view.routed.component';
 import { BalanceAdminEditRoutedComponent } from './component/balance/balance.admin.edit.routed/balance.admin.edit.routed.component';
 import { BalanceAdminPlistRoutedComponent } from './component/balance/balance.admin.plist.routed/balance.admin.plist.routed.component';
+import { BalanceXTipoapunteAdminPlistRoutedComponent } from './component/balance/balance.xtipoapunte.admin.plist.routed/balance.xtipoapunte.admin.plist.routed.component';
+import { BalanceXSubcuentaAdminPlistRoutedComponent } from './component/balance/balance.xsubcuenta.admin.plist.routed /balance.xsubcuenta.admin.plist.routed.component';
+import { BalanceXTipocuentaAdminPlistRoutedComponent } from './component/balance/balance.xtipocuenta.admin.plist.routed/balance.xtipocuenta.admin.plist.routed.component';
+import { BalanceXTipoasientoAdminPlistRoutedComponent } from './component/balance/balance.xtipoasiento.admin.plist.routed/balance.xtipoasiento.admin.plist.routed.component';
 
 import { CuentaAdminDeleteRoutedComponent } from './component/cuenta/cuenta.admin.delete.routed/cuenta.admin.delete.component';
 import { CuentaAdminViewRoutedComponent } from './component/cuenta/cuenta.admin.view.routed/cuenta.admin.view.routed.component';
 import { CuentaAdminEditRoutedComponent } from './component/cuenta/cuenta.admin.edit.routed/cuenta.admin.edit.routed.component';
 import { CuentaAdminPlistRoutedComponent } from './component/cuenta/cuenta.admin.plist.routed/cuenta.admin.plist.routed.component';
+import { CuentaAdminCreateRoutedComponent } from './component/cuenta/cuenta.admin.create.routed/cuenta.admin.create.routed.component';
+import { CuentaXTipoCuentaAdminPlistRoutedComponent } from './component/cuenta/cuenta.xtipocuenta.admin.plist.routed/cuenta.xtipocuenta.admin.plist.routed.component';
 
 import { PeriodoAdminCreateRoutedComponent } from './component/periodo/periodo.admin.create.routed/periodo.admin.create.routed.component';
 import { PeriodoAdminDeleteRoutedComponent } from './component/periodo/periodo.admin.delete.routed/periodo.admin.delete.routed.component';
 import { PeriodoAdminEditRoutedComponent } from './component/periodo/periodo.admin.edit.routed/periodo.admin.edit.routed.component';
 import { PeriodoAdminPlistRoutedComponent } from './component/periodo/periodo.admin.plist.routed/periodo.admin.plist.routed.component';
 import { PeriodoAdminViewRoutedComponent } from './component/periodo/periodo.admin.view.routed/periodo.admin.view.routed.component';
-
-import { AsientoXUsuarioAdminPlistRoutedComponent } from './component/asiento/asiento.xusuario.admin.plist.routed/asiento.xusuario.admin.plist.routed.component';
-import { ApunteXSubcuentaAdminPlistRoutedComponent } from './component/apunte/apunte.xsubcuenta.admin.plist.routed/apunte.xsubcuenta.admin.plist.routed.component';
-import { ApunteXTipoapunteAdminPlistRoutedComponent } from './component/apunte/apunte.xtipoapunte.admin.plist.routed /apunte.xtipoapunte.admin.plist.routed.component';
-import { CuentaXTipoCuentaAdminPlistRoutedComponent } from './component/cuenta/cuenta.xtipocuenta.admin.plist.routed/cuenta.xtipocuenta.admin.plist.routed.component';
-import { ApunteXAsientoAdminPlistRoutedComponent } from './component/apunte/apunte.xasiento.admin.plist.routed/apunte.xasiento.admin.plist.routed.component';
-import { AsientoXTipoasientoAdminPlistRoutedComponent } from './component/asiento/asiento.xtipoasiento.admin.plist.routed/asiento.xtipoasiento.admin.plist.routed.component';
-import { AsientoXPeriodoAdminPlistRoutedComponent } from './component/asiento/asiento.xperiodo.admin.plist.routed/asiento.xperiodo.admin.plist.routed.component';
-import { BalanceXTipoapunteAdminPlistRoutedComponent } from './component/balance/balance.xtipoapunte.admin.plist.routed/balance.xtipoapunte.admin.plist.routed.component';
-import { BalanceXSubcuentaAdminPlistRoutedComponent } from './component/balance/balance.xsubcuenta.admin.plist.routed /balance.xsubcuenta.admin.plist.routed.component';
-import { BalanceXTipocuentaAdminPlistRoutedComponent } from './component/balance/balance.xtipocuenta.admin.plist.routed/balance.xtipocuenta.admin.plist.routed.component';
-import { BalanceXTipoasientoAdminPlistRoutedComponent } from './component/balance/balance.xtipoasiento.admin.plist.routed/balance.xtipoasiento.admin.plist.routed.component';
-
-
 
 export const routes: Routes = [
   { path: '', component: SharedHomeRoutedComponent },
